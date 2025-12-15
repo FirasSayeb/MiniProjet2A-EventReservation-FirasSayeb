303e8ea871f31938a7d6ae2bc1b2f38a5670d704
FirasSayeb <firassayeb2@gmail.com>
2025-12-15 08:51:39 +0100

    adding database connection

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+    try {
+        $dbhost = 'localhost';
+        $dbname='minievent';
+        $dbuser = 'root';
+        $dbpass = '';
+        $conn = new PDO(
+"mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    
+   catch (PDOException $e) {
+        echo "Error : " . $e->getMessage() . "<br/>";
+        die();
+    }
+    
+?>
