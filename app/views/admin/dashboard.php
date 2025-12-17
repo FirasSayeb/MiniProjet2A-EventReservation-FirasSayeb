8dd83a26605292889277bb387a2db78363def261
FirasSayeb <firassayeb2@gmail.com>
2025-12-17 11:11:51 +0100

    adding delete event

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -1,6 +1,51 @@
 <head>
-<link rel="stylesheet" href=" http://localhost/MiniEvent/public/css/dashboard.css">
+<link rel="stylesheet" href="http://localhost/MiniEvent/public/css/style.css">
 </head>
 <?php  require __DIR__ . '/../partials/AdminNavbar.php';  ?>
 
+
+<div>
+    <h1>All Events</h1>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Date</th>
+                <th>Location</th>
+                <th>Seats</th>
+                <th>Image</th>
+                <th colspan="3">Actions</th>
+            </tr>
+        </thead>
+
+        <?php foreach ($events as $event): ?>
+            <tr>
+                <td><?= $event->id ?></td>
+                <td><?= $event->title ?></td>
+                <td><?= $event->description ?></td>
+                <td><?= $event->date ?></td>
+                <td><?= $event->location ?></td>
+                <td><?= $event->seats ?></td>
+                <td><img src="<?= $event->image ?>" alt="event"/></td>
+                  <td>
+               <a class="btn view"  href="http://localhost/MiniEvent/public/admin/events/<?= $event->id ?>">View</a>
+                  <a class="btn edit" href="http://localhost/MiniEvent/public/admin/events/update/<?= $event->id ?>">Modify</a>
+               <a class="btn delete" href="http://localhost/MiniEvent/public/admin/events/delete/<?= $event->id ?>"
+   onclick="return confirm('Are you sure you want to delete this event?')">
+   Delete
+</a>
+
+            
+                
+</td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
+    
+</div>
 <?php require __DIR__ . '/../partials/footer.php';  ?>
+
--- config/routes.php
+++ config/routes.php
@@ -9,4 +9,8 @@ return [
     '/admin/login' => 'AdminController@login',
     '/admin/reservations' => 'AdminController@reservations',
      '/admin/logout' => 'AdminController@logout',
+     '/admin/events/{id}' => 'AdminController@details',
+      '/admin/events/add' => 'AdminController@add',
+      '/admin/events/update/{id}' => 'AdminController@update',
+       '/admin/events/delete/{id}' => 'AdminController@delete',
 ];
